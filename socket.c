02390dfe25930fec85f2cc796df04ec396ea0454
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-09-28 12:40:16 +0200

    Improved network stack

--- socket.c
+++ socket.c
@@ -18,13 +18,14 @@ int term;
 void* SocketThread(){
 	while(!term){
 		//On attend qu'un client se connecte au serveur
-		printf("Waiting for client...\n");
+		printf("\e[32mWaiting for client...\e[m\n");
 		socketClient = accept(socketServer, NULL, NULL);
 		if(socketClient<0){
-			printf("Accept error\n");
+			if(term)return;
+			printf("\e[1;33mAccept error\e[m\n");
 			continue;
 		}
-		printf("Client is connected\n");
+		printf("\e[32mClient is connected\e[m\n");
 
 		//On lit ce qu'il raconte
 		while(!term){
@@ -39,7 +40,7 @@ void* SocketThread(){
 				break;
 		}
 		socketClient = -1;
-		printf("Client closed connection\n");
+		printf("\e[1;33mClient closed connection\e[m\n");
 	}
 	return 0;
 }
@@ -49,38 +50,38 @@ int SocketHandleClients(){
 	term = 0;
 int created = pthread_create(&thread, NULL, SocketThread, NULL);
 	if(created<0){
-		printf("Unable to start socket thread");
+		printf("\e[1;31;43mUnable to start socket thread\e[m");
 		return created;
 	}
 	return 0;
 }
 
 void SocketClose(){
-	printf("Closing sockets\n");
+	printf("\e[33mClosing sockets\e[m\n");
     if(socketClient>=0)
         close(socketClient);
     if(socketServer>=0)
         close(socketServer);
 
-	printf("Socket closed\n");
+	printf("\e[33mSocket closed\e[m\n");
 
 	if(thread!=0){
 
 		term = 1;
 		//pthread_join(thread, NULL);
 	}
-	system("rm /tmp/hwsocket>/dev/null");
+	system("if [ -e /tmp/hwsocket ]\nthen\nrm /tmp/hwsocket\nfi");
 }
 
 
 int SocketInit(){
 	//Suppression de l'ancienne socket
-	system("rm /tmp/hwsocket>/dev/null");
+	system("if [ -e /tmp/hwsocket ]\nthen\nrm /tmp/hwsocket\nfi");
 
 	//Init de la socket
 	socketServer = socket(AF_UNIX, SOCK_SEQPACKET, 0);
 	if(socketServer < 0){
-		printf("Unable to init socket\n");
+		printf("\e[1;31;43mUnable to init socket\e[m\n");
 		return socketServer;
 	}
 
@@ -92,18 +93,18 @@ int SocketInit(){
 	//Lien entre la socket et l'adresse
 	int binded = bind(socketServer, (struct sockaddr*)&addressServer, sizeof(addressServer));
 	if(binded<0){
-		printf("Unable to bind socket\n");
+		printf("\e[1;31;43mUnable to bind socket\e[m\n");
 		return binded;
 	}
 
 	//On écoute la socket pour attendre une connexion
 	int listened = listen(socketServer, 1);
 	if(listened<0){
-		printf("Unable to listen to socket\n");
+		printf("\e[1;31;43mUnable to listen to socket\e[m\n");
 		return listened;
 	}
 
-	printf("Listening to socket /tmp/hwsocket ...\n");
+	printf("\e[32mListening to socket /tmp/hwsocket ...\e[m\n");
 
 	return 0;
 }
@@ -127,7 +128,7 @@ void SocketSendGps(double latitude, double longitude){
 	//lat -90 : 90
 	//lon -180 : 180
 	if(latitude<-90 || latitude>90 || longitude<-180 || longitude>180){
-		printf("WARNING: GPS value out of bound (%f|%f)\n", latitude, longitude);
+		printf("\e[1;33mWARNING: GPS value out of bound (%f|%f)\e[m\n", latitude, longitude);
 		return;
 	}
 
@@ -140,7 +141,7 @@ void SocketSendGps(double latitude, double longitude){
 void SocketSendRoll(double angle){
 	//angle -180 : 180
 	if(angle<-180 || angle>180){
-		printf("WARNING: Roll value out of bound (%f)\n", angle);
+		printf("\e[1;33mWARNING: Roll value out of bound (%f)\e[m\n", angle);
 		return;
 	}
 	struct Event ev;
@@ -152,7 +153,7 @@ void SocketSendRoll(double angle){
 void SocketSendWindDir(double angle){
 	//angle -180 : 180
 	if(angle<-180 || angle>180){
-		printf("WARNING: WindDir value out of bound (%f)\n", angle);
+		printf("\e[1;33mWARNING: WindDir value out of bound (%f)\e[m\n", angle);
 		return;
 	}
 	struct Event ev;
@@ -164,7 +165,7 @@ void SocketSendWindDir(double angle){
 void SocketSendCompass(double angle){
 	//angle 0 : 360
 	if(angle<0 || angle>360){
-		printf("WARNING: Compass value out of bound (%f)\n", angle);
+		printf("\e[1;33mWARNING: Compass value out of bound (%f)\e[m\n", angle);
 		return;
 	}
 	struct Event ev;
@@ -176,7 +177,7 @@ void SocketSendCompass(double angle){
 void SocketSendBattery(float voltage){
 	//voltage 0 : 18
 	if(voltage<0 || voltage>10){
-		printf("WARNING: Battery value out of bound (%f)\n", voltage);
+		printf("\e[1;33mWARNING: Battery value out of bound (%f)\e[m\n", voltage);
 		return;
 	}
 	struct Event ev;
